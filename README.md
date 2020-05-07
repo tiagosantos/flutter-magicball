7bc6912552906d45f2886465655c5f49a0caaf6c
Carlos Tiago <ontiago@gmail.com>
2020-05-06 22:26:19 -0300

    Project Magic Ball

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-![App Brewery Banner](https://github.com/londonappbrewery/Images/blob/master/AppBreweryBanner.png)
-
-
 # Magic 8 Ball 🎱
 
 ## Our Goal
@@ -14,7 +11,3 @@ We’re going to make a magical 8 ball app, it will give you the answers to all 
 
 ![Finished App](https://github.com/londonappbrewery/Images/blob/master/8-ball-flutter-gif.gif)
 
-
->This is a companion project to The App Brewery's Complete Flutter Development Bootcamp, check out the full course at [www.appbrewery.co](https://www.appbrewery.co/)
-
-![End Banner](https://github.com/londonappbrewery/Images/blob/master/readme-end-banner.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.lightBlueAccent,
+          appBar: AppBar(
+            title: Center(
+              child: Text('Ask Me Anything'),
+            ),
+            backgroundColor: Colors.blueAccent,
+          ),
+          body: MagicBall(),
+        ),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int numberTap = 1;
+  void chanceMagicBall() {
+    setState(() {
+      numberTap = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                chanceMagicBall();
+              },
+              child: Image.asset('images/ball$numberTap.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
